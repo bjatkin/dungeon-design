afa7426bf6a003ae440428c42b7884ee9f98478e
RyanKWest <west.ryan.k@gmail.com>
2020-02-13 18:58:36 -0700

    Fix Friction Bug
    Frictional force was being added multiple times for each particle
    instead of once for each particle.

--- graph_to_level/unraveler.py
+++ graph_to_level/unraveler.py
@@ -5,9 +5,9 @@ from graph_to_level.spatial_graph_visualizer import SpatialGraphVisualizer
 
 class Unraveler:
     @staticmethod
-    def unravel(mission_graph_start_node, frame_debug_method=None):
-        mission_nodes, positions, adjacency_matrix = Unraveler.build_spatial_graph(mission_graph_start_node)
-        velocities = np.random.random([len(mission_nodes), 2])
+    def unravel_spatial_graph(node_positions, adjacency_matrix, frame_debug_method=None):
+        node_count, _  = node_positions.shape
+        velocities = np.random.random([node_count, 2])
         time_step = 0.1
         node_mass = 1.0
         debug_info = None
@@ -18,25 +18,25 @@ class Unraveler:
         steps = 0
 
         while sum(offsets_history) / len(offsets_history) > stopping_epsilon and steps < max_step_count:
-            forces = Unraveler.accumulate_forces(positions, velocities, adjacency_matrix)
+            forces = Unraveler.accumulate_forces(node_positions, velocities, adjacency_matrix)
             velocities += forces / node_mass * time_step
             offsets = velocities * time_step
-            positions += offsets
+            node_positions += offsets
 
-            centroid = np.average(positions, axis=0)
-            positions -= centroid
+            centroid = np.average(node_positions, axis=0)
+            node_positions -= centroid
 
             if not frame_debug_method is None:
-                debug_info = frame_debug_method(positions, adjacency_matrix, debug_info, last_frame=False)
+                debug_info = frame_debug_method(node_positions, adjacency_matrix, debug_info, last_frame=False)
 
             offsets_history.pop(0)
             offsets_history.append(np.average(np.linalg.norm(offsets, axis=1)))
             steps += 1
             print("steps {}".format(steps))
 
-        frame_debug_method(positions, adjacency_matrix, debug_info, last_frame=True)
+        frame_debug_method(node_positions, adjacency_matrix, debug_info, last_frame=True)
 
-    first_time = True
+
     @staticmethod
     def accumulate_forces(positions, velocities, adjacency_matrix):
         count, _ = positions.shape
@@ -61,10 +61,11 @@ class Unraveler:
                     forces[i] += force
         
         # Friction Forces
-        for i in range(count):
-            forces -= velocities * 0.1
+        # for i in range(count):
+        #     forces -= velocities * 0.1
+        forces -= velocities * 0.4
 
-        forces += Unraveler.overlap_repulsive_force(positions, adjacency_matrix)
+        # forces += Unraveler.overlap_repulsive_force(positions, adjacency_matrix)
         
         return forces
 
@@ -133,31 +134,3 @@ class Unraveler:
     @staticmethod
     def distance(position_a, position_b):
         return np.linalg.norm(position_a - position_b, axis=0)
-
-    @staticmethod
-    def build_spatial_graph(mission_graph_start_node):
-        nodes = set()
-        Unraveler.find_all_nodes(mission_graph_start_node, nodes)
-        nodes = sorted(nodes, key=lambda x: x.name)
-        node_positions = np.transpose(np.stack([np.arange(len(nodes)), np.zeros(len(nodes))]))
-        # node_positions = np.random.random([len(nodes), 2]) * 10
-        adjacency_matrix = np.zeros([len(nodes), len(nodes)])
-        for i, node_start in enumerate(nodes):
-            for j, node_end in enumerate(nodes):
-                if node_end in node_start.child_s:
-                    adjacency_matrix[(i, j)] = 1
-                    adjacency_matrix[(j, i)] = 1
-        return nodes, node_positions, adjacency_matrix
-
-
-
-    @staticmethod
-    def find_all_nodes(node, nodes_set):
-        nodes_set.add(node)
-        for child in node.child_s:
-            if not child in nodes_set:
-                Unraveler.find_all_nodes(child, nodes_set)
-
-        
-        
-
