f99155e5af00263168e0ee92454c8b7c42348341
RyanKWest <west.ryan.k@gmail.com>
2020-02-27 18:47:28 -0700

    Fix Graph Node Init
    When you pass a list of children/parents/locks/keys
    to the initialization of a node, it shouldn't just
    assign those to its private variable. It should call
    the add_****_s() function so that the connection is
    added bidirectionally.

--- graph_structure/graph_node.py
+++ graph_structure/graph_node.py
@@ -2,14 +2,16 @@ import numpy as np
 
 class GNode(object):
     def __init__(self, name="", parent_s=None, child_s=None):
-        if child_s == None:
-            child_s = []
-        if parent_s == None:
-            parent_s = []
+        self.child_s = []
+        self.parent_s = []
 
-        self.child_s = child_s
-        self.parent_s = parent_s
+        if child_s is not None:
+            self.add_child_s(child_s)
+        if parent_s is not None:
+            self.add_parent_s(parent_s)
+
         self.name = name
+
 
     # traversal_order=['preorder','postorder']
     @staticmethod
@@ -33,6 +35,7 @@ class GNode(object):
         if traversal_order == 'postorder':
             visit_method(node, visited_nodes)
 
+
     @staticmethod
     def traverse_nodes_breadth_first(node, visit_method, will_traverse_method = lambda node, visited_nodes: True):
         visited_nodes = set()
@@ -54,9 +57,11 @@ class GNode(object):
     def find_all_nodes(node, method="depth-first"):
         visited = []
 
+
         def visit_method(node, visited_nodes):
             nonlocal visited
             visited.append(node)
+
 
         if method == "depth-first":
             GNode.traverse_nodes_depth_first(node, visit_method)
@@ -68,11 +73,13 @@ class GNode(object):
 
         return visited
 
+
     @staticmethod
     def _listify(items):
         if not isinstance(items, list):
             items = [items]
         return items
+
 
     @staticmethod
     def _add(_self, items, get_self_list_method, get_items_list_method):
@@ -90,17 +97,21 @@ class GNode(object):
         for item in items:
             get_items_list_method(item)[:] = [item for item in get_items_list_method(item) if item.name != _self.name]
 
+
     def add_child_s(self, child_s):
         # Instead of having the code to add a child/parent/key here, we call a static method
         # passing it the list that we want to modify.
         GNode._add(self, child_s, lambda x: x.child_s, lambda x: x.parent_s)
     
+
     def remove_child_s(self, child_name_s):
         GNode._remove(self, child_name_s, lambda x: x.child_s, lambda x: x.parent_s)
     
+
     def add_parent_s(self, parent_s):
         GNode._add(self, parent_s, lambda x: x.parent_s, lambda x: x.child_s)
     
+
     def remove_parent_s(self, parent_name_s):
         GNode._remove(self, parent_name_s, lambda x: x.parent_s, lambda x: x.child_s)
 
@@ -112,51 +123,45 @@ class GNode(object):
         string = "{}(\'{}\', parents={}, children={})".format(class_name, self.name, parent_names, child_names)
         return string
 
+
     def __eq__(self, other): 
         return str(self) == str(other)
+
 
     def __hash__(self):
         return hash(str(self))
 
 
+
 class Start(GNode):
     def __init__(self, child_s=None):
-        if child_s == None:
-            child_s = []
-
         super(Start, self).__init__("Start", [], child_s)
 
 
 class Key(GNode):
     def __init__(self, name="", parent_s=None, child_s=None, lock_s=None):
-        if child_s == None:
-            child_s = []
-        if parent_s == None:
-            parent_s = []
-        if lock_s == None:
-            lock_s = []
+        self.lock_s = []
+        if lock_s is not None:
+            self.add_lock_s(lock_s)
 
         super(Key, self).__init__(name, parent_s, child_s)
-        self.lock_s = lock_s
     
+
     def add_lock_s(self, lock_s):
         GNode._add(self, lock_s, lambda x: x.lock_s, lambda x: x.key_s)
     
+
     def remove_lock_s(self, lock_name_s):
         GNode._remove(self, lock_name_s, lambda x: x.lock_s, lambda x: x.key_s)
         
 
 class Lock(GNode):
     def __init__(self, name="", parent_s=None, child_s=None, key_s=None):
-        if child_s == None:
-            child_s = []
-        if parent_s == None:
-            parent_s = []
-        if key_s == None:
-            key_s = []
+        self.key_s = []
+        if key_s is not None:
+            self.add_key_s(key_s)
 
         super(Lock, self).__init__(name, parent_s, child_s)
-        self.key_s = key_s
     
     def add_key_s(self, key_s):
         GNode._add(self, key_s, lambda x: x.key_s, lambda x: x.lock_s)
